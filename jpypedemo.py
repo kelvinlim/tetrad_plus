71d8bcbdc681b54adda985e12cb037e57056be1d
Kelvin O. Lim <kolim@umn.edu>
2025-05-22 06:54:37 -0500

    initial

---
+++ jpypedemo.py
@@ -0,0 +1,80 @@
+import os
+import pandas as pd
+import numpy as np
+
+import jpype
+import jpype.imports
+
+# REPLACE WITH YOUR PATH TO JAR
+jpype.startJVM("-Xmx4g", classpath="jars/tetrad-current.jar")
+
+import java.util as util
+import edu.cmu.tetrad.data as td
+import edu.cmu.tetrad.graph as tg
+import edu.cmu.tetrad.search as ts
+
+
+# HELPER FUNCTION TO CONVERT A PANDAS DF
+# TO A JAVA DATASET; I USUALLY COPY PASTE THIS
+# INTO ALL CDA PROJECTS
+def df_to_data(df):
+    cols = df.columns
+    values = df.values
+    n, p = df.shape
+
+    # JITTER THE DATA; FEEL FREE TO REMOVE
+    values += 1e-3 * np.random.randn(n, p)
+
+    variables = util.ArrayList()
+    for col in cols:
+        variables.add(td.ContinuousVariable(str(col)))
+
+    databox = td.DoubleDataBox(n, p)
+    for col, var in enumerate(values.T):
+        for row, val in enumerate(var):
+            databox.set(row, col, val)
+
+    return td.BoxDataSet(databox, variables)
+
+
+### START SEARCH PARAMETERS ###
+penalty_discount = 1
+boss_bes = False
+boss_starts = 10
+boss_threads = 8
+### END SEARCH PARAMETERS ###
+
+
+# IN THIS EXAMPLE DATA CONTAINS MANY DATASETS
+path = "data/"
+for fname in ['boston_data_raw.csv']:
+    print(fname)
+    df = pd.read_csv(path + fname)
+    data = df_to_data(df)
+
+    # MAKE TIERED KNOWLEDGE BASED ON LAGS
+    knowledge = td.Knowledge()
+    for col in df.columns:
+        if col[-4:] == "_lag": knowledge.addToTier(0, col)
+        else:knowledge.addToTier(1, col)
+    knowledge.setTierForbiddenWithin(0, True)
+
+    score = ts.score.SemBicScore(data, True)
+    score.setPenaltyDiscount(penalty_discount)
+    score.setStructurePrior(0)
+
+    boss = ts.Boss(score)
+    boss.setUseBes(boss_bes)
+    boss.setNumStarts(boss_starts)
+    boss.setNumThreads(boss_threads)
+    boss.setUseDataOrder(False)
+    boss.setResetAfterBM(False)
+    boss.setResetAfterRS(False)
+    boss.setVerbose(False)
+
+    search = ts.PermutationSearch(boss)
+    search.setKnowledge(knowledge)
+
+    # SAVE THE OUTPUT GRAPHS
+    graph = search.search().toString()
+    with open(f"graphs/{fname}", "w") as f: f.write(str(graph))
